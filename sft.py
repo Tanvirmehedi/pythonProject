f8e9077501f2c63c1ef93f6bde22f7f71819d028
Tanvirmehedi <tjmehedi201@gmail.com>
2021-10-09 23:29:44 +0600

    Update sft.py

--- sft.py
+++ sft.py
@@ -1,6 +1,5 @@
 import math
 
-
 class Stone_measurement:
     def __init__(self, height, length, width):
         self.height = height
@@ -15,9 +14,17 @@ class Stone_measurement:
             conv_width_str = str(self.width)
             text = conv_height_str.split('.'), conv_length_srt.split(
                 '.'), conv_width_str.split('.')
+            num = 1
             for item in text:
-                result = int(item[1])/12
-        return "%.2f" % result
+                division_result = int(item[1])/12
+                division_result_formation = "%.2f" % division_result
+                result_str = str(division_result_formation)
+                explode = result_str.split('.')
+                join_num = f"{item[0]}.{explode[1]}"
+                conv_float2num = float(join_num)
+                num = num*conv_float2num
+                formate_num ="%.2f"%num
+            return f"\nThe result is {formate_num} feet\n"
 
 
 stone_input = Stone_measurement(float(input("Input Height : ")), float(
